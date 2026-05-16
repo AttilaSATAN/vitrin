36d0dd2d31c92040188af0931e614cde9be0b8cc
Attila Satan <attila.satan@gmail.com>
2026-05-16 16:22:42 +0300

    add more CORS for different enviroiments

--- backend/src/main/java/com/tusas/vitrin/infrastructure/config/SecurityConfig.java
+++ backend/src/main/java/com/tusas/vitrin/infrastructure/config/SecurityConfig.java
@@ -86,6 +86,8 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
+                "http://localhost",
+                "http://localhost:80",
                 "http://localhost:5173",
                 "http://localhost:3000"
         ));
